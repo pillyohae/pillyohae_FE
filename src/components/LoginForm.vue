720202ae2e49647677f4bbda7da38b8ce42b5407
Weseunghyun <bible091192@gmail.com>
2025-01-13 10:28:13 +0900

    feat: 로그인 화면 구현 Form 과 View 로 나누어 책임이 너무 커지지 않도록 구현 재사용성 증가. 자세한 코드 설명은 주석 참고

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -1,13 +1,51 @@
 <template>
-  
+    <!-- 로그인 양식 Vuetify 에서 제공하는 컴포넌트로 구현 
+   @ 는 Vue 의 이벤트 바인딩, @submit 은 submit 이벤트가 발생할 때 실행할 함수를 바인딩
+   .prevent는 Vue 의 이벤트 수식어로 기본동작을 방지, form 태그가 데이터를 전송할 때
+   submit 을 호출하고 페이지를 새로고침하는데 그것을 Vue에서만 handleLogind으로 동작하도록 막음-->
+    <v-form @submit.prevent="handleLogin">
+        <!-- 이메일 입력 텍스트 필드 v-model 은 HTML 폼 요소값과 Vue 컴포넌트 데이터를 연결
+     v-model 의 email 은 컴포넌트의 data() 에 정의된 변수. 사용자가 입력필드에 값을 입력하면
+     data()의 email 변수에 해당 값이 저장됨-->
+        <v-text-field v-model.trim="email" label="이메일" type="email" required />
+
+        <!-- 패스워드 입력 텍스트 필드-->
+        <v-text-field v-model.trim="password" label="패스워드" type="password" required />
+
+        <!-- 로그인 버튼 button 태그의 속성을 submit 으로 두면 폼 제출 버튼을 의미
+     form 태그의 데이터를 서버 or 부모 컴포넌트에 전달, 클릭 시 submit 이벤트가 발생함.
+      block과 class 는 css 요소-->
+        <v-btn type="submit" color="green" block class="mt-4">로그인</v-btn>
+
+        <!-- 회원가입으로 이동하는 버튼 -->
+        <v-btn type="button" color="green" block class="mt-4" @click="goTosignUp">회원가입</v-btn>
+    </v-form>
 </template>
 
 <script>
-export default {
 
+//JS 에서 모듈을 내보낼 때 사용하는 문법 export default, Vue 파일에서 컴포넌트의 설정 객체를 내보냄
+//내보낼 수 있는 정보 : 데이터, 메서드, 라이프 사이클 훅 등을 정의
+//해당 Vue 파일을 다른 파일에서 사용할 수 있도록 내보내기 위해서 사용
+export default {
+    data() {
+        return {
+            email: '', //사용자가 입력한 이메일
+            password: '', //사용자가 입력한 패스워드
+        };
+    },
+    methods: {
+        handleLogin() {
+            //emit 은 Vue 인스턴스의 메서드, 부모 컴포넌트에 이벤트를 발생시켜 데이터를 전달
+            //자식 -> 부모 : emit, 부모 -> 자식 : props
+            //첫번째 인자는 이벤트 이름, 두번째 인자는 전달할 데이터.
+            this.$emit('login', { email: this.email, password: this.password })
+        },
+        goTosignUp() {
+            this.$router.push('/signup');
+        }
+    }
 }
 </script>
 
-<style>
-
-</style>
+<style></style>
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,13 +1,59 @@
 <template>
-  
+    <div>
+        <h2>로그인</h2>
+        <!-- 자식 컴포넌트인 LoginForm 에서 this.$emit 으로 발생한 이벤트를 받아와 메서드로 처리함을 명시-->
+        <LoginForm @login="loginUser" />
+    </div>
 </template>
 
 <script>
-export default {
+import LoginForm from '../components/LoginForm.vue';
+import axios from '../api/axios';
 
+export default {
+    components: { LoginForm },
+    methods: {
+        //자식컴포넌트가 보낸 데이터를 credentials 로 받아옴. 다른 이름이어도 됨.
+        //async는 비동기 함수를 만들어줌. Promise 객체를 반환하며 내부에서 비동기 작업을 처리 가능하게 됨
+        async loginUser(credentials) {
+            try {
+                //await 는 Promise 객체가 처리될 때까지 기다렸다가 결과를 반환함, 서버가 응답을 반환할 때까지 기다림
+                //반드시 async 가 붙은 비동기 함수 내에서만 사용할 수 있음
+                const response = await axios.post('/users/login', credentials);
+
+                // 서버의 응답인 JWT 토큰을 헤더에서 가져옴
+                const token = response.headers.authorization;
+
+                // 토큰에서 정보 추출을 위해 디코딩
+                // 토큰의 정보가 담긴 가운데부분 payload 를 디코딩
+                const payload = JSON.parse(atob(token.split(".")[1]));
+
+                // 토큰과 사용자 정보가 있다면 실행
+                if (token && payload) {
+                    //로컬 스토리지에 토큰을 저장
+                    localStorage.setItem('accessToken', token);
+                    console.log('로그인 성공 : ', token);
+                    alert('로그인 성공!');
+
+                    //payload 에 담긴 사용자의 역할에 따라 화면 이동을 다르게 함
+                    if (payload.role === 'SELLER') {
+                        this.$router.push('/seller') //판매자 메인으로 이동
+                    } else if (payload.role === 'BUYER') {
+                        this.$router.push('/buyer'); //구매자 메인으로 이동
+                    } else {
+                        alert('알 수 없는 사용자 권한입니다.');
+                    }
+                } else {
+                    console.error('Authorization 헤더에 토큰이 없습니다.');
+                }
+            } catch (error) {
+                //로그인 요청이 실패했을 때 실행
+                console.error('로그인 실패 : ', error.response?.data || error.message);
+                alert('로그인 실패. 다시 시도해주세요!');
+            }
+        },
+    }
 }
 </script>
 
-<style>
-
-</style>
+<style></style>
